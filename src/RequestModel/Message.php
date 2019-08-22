e3b3438a7eb3dac7c8e41b8eae0bdf91f9b4d433
Robert Šípek <sipek@mzk.cz>
2019-08-22 13:27:59 +0200

    Update Message model

--- src/RequestModel/Message.php
+++ src/RequestModel/Message.php
@@ -1,8 +1,6 @@
 <?php declare(strict_types = 1);
 
-
 namespace Mzk\ZiskejApi\RequestModel;
-
 
 class Message
 {
@@ -23,17 +21,14 @@ class Message
      */
     public function toArray(): array
     {
-        $return = [
+        return [
             'text' => $this->text,
         ];
-
-        return $return;
     }
 
     public function getText(): string
     {
         return $this->text;
     }
-
 
 }
