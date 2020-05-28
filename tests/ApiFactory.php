54d20886646741377dd4d053d35a5f52e16bb013
Robert Sipek <sipek@mzk.cz>
2020-05-28 05:46:05 +0200

    Test JWT

--- tests/ApiFactory.php
+++ tests/ApiFactory.php
@@ -3,26 +3,31 @@
 namespace Mzk\ZiskejApi;
 
 use Http\Message\Authentication\Bearer;
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Signer\Ecdsa\Sha512;
+use Lcobucci\JWT\Signer\Key;
 use Monolog\Logger;
-use Symfony\Component\Dotenv\Dotenv;
 
 class ApiFactory
 {
 
     public static function createApi(): Api
     {
-        $api = new Api(
-            new ApiClient(
-                null,
+        $signer = new Sha512();
+        $privateKey = new Key('file://'. __DIR__ . '/.private/cert-cpk-ziskej-api.key');
+        $time = time();
                 'https://ziskej-test.techlib.cz/api/v1',
                 null,
                 new Logger('ZiskejApi')
             )
         );
 
-
-        $dotEnv = new Dotenv();
-        $dotEnv->load(__DIR__.'/.env');
+        $token = (new Builder())
+            ->issuedBy('cpk') // Configures the issuer (iss claim)
+            ->issuedAt($time) // Configures the time that the token was issue (iat claim)
+            ->expiresAt($time + 3600) // Configures the expiration time of the token (exp claim)
+            ->withClaim('app', 'cpk')
+            ->getToken($signer, $privateKey); // Retrieves the generated token
 
         $token = $api->login($_ENV['username'], $_ENV['password']);
 
