08fb000702e1a253ace2861a28f4f1c236b78e97
Robert Sipek <sipek@mzk.cz>
2022-09-29 12:43:00 +0200

    Fix typing errors

--- src/Enum/StatusName.php
+++ src/Enum/StatusName.php
@@ -8,11 +8,11 @@ abstract class StatusName extends BaseEnum
 {
     public const CREATED = 'created';
     public const PAID = 'paid';
-    public const UUPAID = 'unpaid';
+    public const UNPAID = 'unpaid';
     public const ACCEPTED = 'accepted';
     public const PREPARED = 'prepared';
     public const LENT = 'lent';
     public const CLOSED = 'closed';
-    public const CACNELLED = 'cancelled';
+    public const CANCELLED = 'cancelled';
     public const REJECTED = 'rejected';
 }
